48dad830b855b907e05f1868c896939a5e3e3aa4
Ayomide Adeagbo <ayo2adeagbo@gmail.com>
2026-04-07 16:45:14 +0100

    Initial commit

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>Welcome to my single-page app!</p>
+    </div>
+  );
+}
+
+export default App;
